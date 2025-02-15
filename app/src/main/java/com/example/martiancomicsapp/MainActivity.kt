895368717f96fa367126c60d936c8ed5f0bc40a6
kthurman59 <kthurman59@gmail.com>
2025-02-15 15:23:22 -0600

    MainActivity updated

--- app/src/main/java/com/example/martiancomicsapp/MainActivity.kt
+++ app/src/main/java/com/example/martiancomicsapp/MainActivity.kt
@@ -1,20 +1,19 @@
-package com.yourcompany.martiancomics
+package com.example.martiancomicsapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import com.yourcompany.martiancomics.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import com.example.martiancomicsapp.ui.ComicsListScreen
 
-class MainActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Set the layout defined in res/layout/activity_main.xml
-        setContentView(R.layout.activity_main)
-
-        // Example: Change the welcome text
-        val welcomeText: TextView = findViewById(R.id.welcomeTextView)
-        welcomeText.text = "Welcome to Martian Comics!"
+        setContent {
+            MaterialTheme {
+                ComicsListScreen()
+            }
+        }
     }
 }
+
